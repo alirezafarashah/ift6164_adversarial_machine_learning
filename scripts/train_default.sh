844c336201c5708d3297e41a64feae2dc4d981e2
TheRealSal <75696327+TheRealSal@users.noreply.github.com>
2026-01-20 14:36:55 -0500

    Changed memory requested

--- scripts/train_default.sh
+++ scripts/train_default.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --gpus=h100:1
 #SBATCH --output=/scratch/%u/gpt2-logs/%x-%j.out
 #SBATCH --error=/scratch/%u/gpt2-logs/%x-%j.err
